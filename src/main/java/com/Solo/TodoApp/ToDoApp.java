e01a5f59552beb2fa4255027099848aa01480781
jiyoonchol <jiyoon276479@gmail.com>
2023-02-14 22:22:40 +0900

    dev commit 2

--- src/main/java/com/Solo/TodoApp/ToDoApp.java
+++ src/main/java/com/Solo/TodoApp/ToDoApp.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ToDoApp {
     @GetMapping
-
+    public String getToDoApp() {
+        return "To-Do-Application!";
     }
 }
